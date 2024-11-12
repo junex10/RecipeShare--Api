45f66ac0a6015674eff38cebbc694011555a390a
junex10 <43711689+junex10@users.noreply.github.com>
2024-11-12 00:01:48 -0500

    .

--- src/config.ts
+++ src/config.ts
@@ -3,7 +3,7 @@ require('dotenv').config();
 module.exports = {
    dialect: 'mysql',
    host: 'localhost',
-   port: 3306,
+   port: process.env.DB_PORT,
    username: process.env.DB_USERNAME,
    password: process.env.DB_PASSWORD,
    database: process.env.DB_DATABASE,
